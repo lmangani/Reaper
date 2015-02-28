d00cbc1892f2e25c0232ad5a799918b5eb9329e1
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2015-02-28 19:27:05 +0100

    Split and process RTCP reports

--- src/com/tt/reaper/call/CallManager.java
+++ src/com/tt/reaper/call/CallManager.java
@@ -14,6 +14,16 @@ import com.tt.reaper.message.MessageQueue;
 import com.tt.reaper.message.NotifyMessage;
 import com.tt.reaper.message.RtpPacket;
 import com.tt.reaper.message.SipMessage;
+import com.tt.reaper.rtcp.DataPacket;
+
+import com.tt.reaper.rtcp.RtcpExtendedReport;
+import com.tt.reaper.rtcp.RtcpPacket;
+import com.tt.reaper.rtcp.RtcpReceiverReport;
+import com.tt.reaper.rtcp.RtcpSenderReport;
+import com.tt.reaper.rtcp.VoipMetricsExtendedReportBlock;
+import com.tt.reaper.sip.CollectorStack;
+import com.tt.reaper.vq.LocalMetrics;
+import com.tt.reaper.vq.VQIntervalReport;
 
 public class CallManager extends Thread {
 	protected static Logger logger = Logger.getLogger(CallManager.class);
@@ -49,29 +59,92 @@ public class CallManager extends Thread {
 	{
 		try {
 			if (message instanceof NotifyMessage) {
-				RtpPacket packet;
-				factory.init(((NotifyMessage)message).getRequest().getRawContent());
-				while ((packet = factory.getNext()) != null)
-				{
-					CallContext context;
-					if ((context = rtcpMap.get(packet.getSource())) != null) {
-						logger.debug("Found source: " + packet.getSource());
-						if (context.process(packet) == false) {
-							logger.warn("Removing rtcp map that should of been removed");
-							rtcpMap.remove(packet.getSource());
+				String RURI = ((NotifyMessage)message).getRequest().getRequestURI().toString();
+                		// logger.debug("Notify Reporter: " + RURI );
+				if ( RURI.equals("sip:rtcp.example.com") ) {
+		                        // logger.debug("RTCP PACKET");
+					DataPacket packet = new DataPacket( ((NotifyMessage)message).getRequest().getRawContent() );
+					Iterator<RtcpPacket> it = packet.getIterator();
+					while (it.hasNext()) {
+						CallContext context;
+						context = rtcpMap.get(packet.getSource());
+						RtcpPacket rtcp = it.next();
+							switch (rtcp.getPacketType()) {
+						case RtcpPacket.TYPE_SOURCE_DESCRIPTION:
+							break;
+						case RtcpPacket.TYPE_SENDER_REPORT:
+							RtcpSenderReport sender = (RtcpSenderReport)rtcp;
+							for (int j=0; j<sender.getCount(); j++) {
+								LocalMetrics metrics = context.createMetrics(packet);
+								if (metrics == null)
+									break;
+								metrics.setMetrics(sender.getReport(j));
+								CollectorStack.instance.sendMessage(new VQIntervalReport(metrics).toString());
+							}
+							break;
+						case RtcpPacket.TYPE_RECEIVER_REPORT:
+							RtcpReceiverReport receiver = (RtcpReceiverReport)rtcp;
+							for (int j=0; j<receiver.getCount(); j++) {
+								LocalMetrics metrics = context.createMetrics(packet);
+								if (metrics == null)
+									break;
+								metrics.setMetrics(receiver.getReport(j));
+								CollectorStack.instance.sendMessage(new VQIntervalReport(metrics).toString());
+							}
+							break;
+						case RtcpPacket.TYPE_GOODBYE:
+							// Unregister
+							break;
+						case RtcpPacket.TYPE_EXTENDED_REPORT:
+							RtcpExtendedReport extendedReport = (RtcpExtendedReport)rtcp;
+							Iterator<VoipMetricsExtendedReportBlock> eit = extendedReport.getIterator();
+							while (eit.hasNext()) {
+								LocalMetrics metrics = context.createMetrics(packet);
+								if (metrics == null)
+									break;
+								metrics.setMetrics(eit.next());
+								CollectorStack.instance.sendMessage(new VQIntervalReport(metrics).toString());
+							}
+							break;
+						case RtcpPacket.TYPE_APPLICATION_DEFINED:
+							break;
 						}
 					}
-					else if ((context = rtcpMap.get(packet.getDestination())) != null) {
-						logger.debug("Found destination: " + packet.getDestination());
-						if (context.process(packet) == false) {
-							logger.warn("Removing rtcp map that should of been removed");
-							rtcpMap.remove(packet.getDestination());
+
+				} else {
+	                		logger.debug("RTP PACKET" );
+					RtpPacket packet;
+					factory.init(((NotifyMessage)message).getRequest().getRawContent());
+					while ((packet = factory.getNext()) != null)
+					{
+						CallContext context;
+						if ((context = rtcpMap.get(packet.getSource())) != null) {
+							logger.debug("Found rtp source: " + packet.getSource());
+							if (context.process(packet) == false) {
+								logger.warn("Removing rtp map that should of been removed");
+								rtcpMap.remove(packet.getSource());
+							}
 						}
-					}
-					else {
-						logger.warn("RTCP stream not found: " + packet);
+						else if ((context = rtcpMap.get(packet.getDestination())) != null) {
+							logger.debug("Found rtp destination: " + packet.getDestination());
+							if (context.process(packet) == false) {
+								logger.warn("Removing rtp map that should of been removed");
+								rtcpMap.remove(packet.getDestination());
+							}
+						}
+						else {
+							logger.warn("RTP stream not found: " + packet);
+	
+						}
 					}
 				}
+
+
+			}
+			else if (message instanceof DataPacket) {
+				logger.debug("Found DataPacket: " + message);
+				DataPacket dataPacket = (DataPacket)message;
+				// dataPacket.queue.add(new DataPacket(toString()));
 			}
 			if (message instanceof SipMessage) {
 				SipMessage sipMessage = (SipMessage)message;
@@ -89,6 +162,7 @@ public class CallManager extends Thread {
 				}
 			}
 			else if (message instanceof DataRequest) {
+				logger.debug("Found DataRequest: " + message);
 				DataRequest request = (DataRequest)message;
 				request.queue.add(new DataResponse(toString()));
 			}
@@ -136,7 +210,15 @@ public class CallManager extends Thread {
 	public void send(Message message) {
 		queue.add(message);
 	}
-	
+
+	public void sendrtcp(CallContext context, Message message) {
+		logger.debug("Fake RTCP DATA SEND: "+message);
+		// DataPacket dataPacket = (DataPacket)message;
+		//processDataPacket(context, (DataPacket)message);
+
+
+	}
+
 	public String toString()
 	{
 		String response = "";
